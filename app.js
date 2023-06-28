37c2a24e80cd5a80778089dbc094e34bdafd48fe
KyleHS <k15156@yahoo.com>
2023-06-28 14:53:10 -0600

    Added functionality to all 3 buttons (p1, p2, and reset) through JavaScript.

---
+++ app.js
@@ -0,0 +1,38 @@
+const p1Button = document.querySelector('#p1Button'); //variable for p1 button
+const p2Button = document.querySelector('#p2Button'); //variable for p2 button
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let isGameOver = false;
+
+p1Button.addEventListener('click', function () {  //p1 button functionality
+    if (!isGameOver) {
+        p1Score += 1;
+        if (p1Score === winningScore) {
+            isGameOver = true;
+    }
+    p1Display.textContent = p1Score;
+    }
+});
+
+p2Button.addEventListener('click', function () {  //p2 button functionality
+    if (!isGameOver) {
+        p2Score += 1;
+        if (p2Score === winningScore) {
+            isGameOver = true;
+    }
+    p2Display.textContent = p2Score;
+    }
+});
+
+reset.addEventListener('click', function() {  //reset button functionality
+    p1Score = 0;
+    p1Display.textContent = p1Score;
+    p2Score = 0;
+    p2Display.textContent = p2Score;
+    isGameOver = false;
+})
